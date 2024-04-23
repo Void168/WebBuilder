824c651250381fd8fcdd64d48a39bc59f5aec0cc
LPHung <phuhung16820@gmail.com>
2024-04-23 21:51:56 +0700

    setup subdomain middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,31 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-    publicRoutes: ['site', '/api/uploadthing']
-})
+  publicRoutes: ["site", "/api/uploadthing"],
+
+  async beforeAuth(auth, req) {},
+  async afterAuth(auth, req) {
+    const url = req.nextUrl;
+    const searchParams = url.searchParams.toString();
+    let hostname = req.headers;
+
+    const pathWithSearchParams = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ""
+    }`;
+
+    const customSubDomain = hostname
+      .get("host")
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0];
+
+    if (customSubDomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${pathWithSearchParams}`)
+      );
+    }
+  },
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
