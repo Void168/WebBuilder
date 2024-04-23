bd470cdf40b38dc2dadd438c1adefd50d1205f23
LPHung <phuhung16820@gmail.com>
2024-04-23 11:13:17 +0700

    setup web builder project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'uploadthing.com',
+            'utfs.com',
+            'img.clerk.com',
+            'subdomain',
+            'files.stripe.com'
+        ]
+    }
+};
 
 export default nextConfig;
