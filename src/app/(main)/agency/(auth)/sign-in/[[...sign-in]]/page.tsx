1739921213362324cbca48fe1424cd1983b26a1b
LPHung <phuhung16820@gmail.com>
2024-04-24 14:03:05 +0700

    get auth user details

--- src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,9 @@ import { SignIn } from "@clerk/nextjs";
 
 const Page = () => {
     return ( 
+        <div className='flex justify-center items-center h-screen'>
         <SignIn />
+        </div>
      );
 }
  
