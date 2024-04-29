280f59dfd21dc3e387c834c30dfe69c2bba7524d
LPHung <phuhung16820@gmail.com>
2024-04-29 23:34:41 +0700

    create pipelines page

---
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/page.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/libs/db";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: { subaccountId: string };
+};
+
+const Pipelines = async ({ params }: Props) => {
+  const pinelineExists = await db.pipeline.findFirst({
+    where: {
+      subAccountId: params.subaccountId,
+    },
+  });
+
+  if (pinelineExists) {
+    return redirect(
+      `/subaccount/${params.subaccountId}/pipelines/${pinelineExists.id}`
+    );
+  }
+
+  try {
+    const response = await db.pipeline.create({
+      data: {
+        name: "First Pineline",
+        subAccountId: params.subaccountId,
+      },
+    });
+
+    return redirect(
+      `/subaccount/${params.subaccountId}/pipelines/${response.id}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  return <div>Pipelines</div>;
+};
+
+export default Pipelines;
